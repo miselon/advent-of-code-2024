3e7fdcf6f36dbe8134b12eeb9c7189f3dd7f359a
miselon <miselon@github.com>
2024-12-11 22:34:45 +0100

    day 1, part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,32 +1,48 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn main() -> std::io::Result<()> {
+fn main() {
 
-    let file = File::open("C:\\Users\\Mateusz\\Documents\\workspace\\advent-of-code-2024\\day1\\src\\input")?;
-    let reader = io::BufReader::new(file);
+    let lists = get_sorted_input_data();
 
-    let mut leftList: Vec<i32> = vec![];
-    let mut rightList: Vec<i32> = vec![];
+    println!("Total distance between location IDs is: {}", get_total_distance_between_lists(&lists));
+    println!("Similarity score of location IDs is: {}", get_similarity_score(lists));
+}
 
-    for line in reader.lines() {
+fn get_similarity_score(lists:(Vec<i32>, Vec<i32>)) -> i32 {
 
-        let line = line?;
-        let parts: Vec<&str> = line.split_terminator("   ").collect();
+    let lists_length = lists.0.len();
+    let mut similarity_score = 0;
 
-        leftList.push(parts[0].parse().unwrap());
-        rightList.push(parts[1].parse().unwrap());
+    for left_index in 0 .. lists_length {
+
+        let left = lists.0[left_index];
+        let mut occurences = 0;
+
+        for right_index in 0 .. lists_length {
+
+            let right = lists.1[right_index];
+    
+            if left == right {
+
+                occurences += 1;
+            }
+        }
+
+        similarity_score += left * occurences;
     }
 
-    leftList.sort();
-    rightList.sort();
+    similarity_score
+}
+
+fn get_total_distance_between_lists(lists:&(Vec<i32>, Vec<i32>)) -> i32 {
 
     let mut sum = 0;
 
-    for (index, value) in leftList.iter().enumerate() {
+    for index in 0 .. lists.0.len() {
 
-        let left = leftList[index];
-        let right = rightList[index];
+        let left = lists.0[index];
+        let right = lists.1[index];
 
         let mut diff = left - right;
 
@@ -38,8 +54,28 @@ fn main() -> std::io::Result<()> {
         sum = sum + diff;
     }
 
-    println!("sum is: {}", sum);
-
-    Ok(())
+    sum
 }
 
+fn get_sorted_input_data() -> (Vec<i32>, Vec<i32>) {
+
+    let file = File::open("input-data").unwrap();
+    let reader = io::BufReader::new(file);
+
+    let mut left_list: Vec<i32> = vec![];
+    let mut right_list: Vec<i32> = vec![];
+
+    for line in reader.lines() {
+
+        let line = line.unwrap();
+        let parts: Vec<&str> = line.split_terminator("   ").collect();
+
+        left_list.push(parts[0].parse().unwrap());
+        right_list.push(parts[1].parse().unwrap());
+    }
+
+    left_list.sort(); 
+    right_list.sort();
+
+    (left_list, right_list)
+}
